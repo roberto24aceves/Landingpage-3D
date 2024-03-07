54b10a95bfb3a2a928a23dd2195aa09fa4c89865
Roberto Aceves <roberto24aceves@outlook.es>
2024-03-07 08:55:20 -0600

    update view

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import './App.css'
 function App() {
   return (
       <main>
-          <iframe className="render" src='https://my.spline.design/anahuacbottle-f33599d38a82604e5e3f259fb60db9ab/' frameBorder='0'
-                  width='100%' height='100%'></iframe>
-          <div className="content">
-              <h3>Yetti Anahuac</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur <br/> adipiscing elit. </p>
-              <button>Comprar ahora!</button>
-          </div>
+          <iframe className="render"
+                  src='https://my.spline.design/anahuacbottle-f33599d38a82604e5e3f259fb60db9ab/' frameBorder='0'
+                  width='100%'
+                  height='100%'></iframe>
       </main>
   )
 }
